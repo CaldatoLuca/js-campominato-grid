68720bc7a41b6c2d03d808563db39fbc316b5c1e
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-15 19:17:03 +0100

    feat: eventi click su bottoni e sulle caselle

--- js/main.js
+++ js/main.js
@@ -13,10 +13,9 @@
 ?--------
 */
 
-function elementHtmlCreator(tag, style, content) {
+function elementHtmlCreator(tag, style) {
   const elementHtml = document.createElement(tag);
   elementHtml.classList.add(style);
-  elementHtml.append(content);
   return elementHtml;
 }
 
@@ -28,18 +27,36 @@ function elementHtmlCreator(tag, style, content) {
 
 //*variabili elemento html
 const elementCellsContainer = document.querySelector(".cells-container");
-const elementButton = document.querySelector(".btn");
-const elementMessage = document.getElementById("message");
+const elementButtonPlay = document.getElementById("play");
+const elementButtonReset = document.getElementById("reset");
+const elementMessage = document.querySelector(".messages");
 
 //* ciclo for per inserire le celle nel container (richiama la funzione che le crea e poi inserisce)
 for (let i = 1; i <= 100; i++) {
-  const elementCell = elementHtmlCreator("div", "cell", i);
+  const elementCell = elementHtmlCreator("div", "cell");
+  elementCell.append(i);
   elementCellsContainer.append(elementCell);
 }
 
+//* creo falso array che raccoglie tutte le mie celle
+const cellsAll = document.querySelectorAll(".cell");
+
 //* evento click sul bottone
-elementButton.addEventListener("click", function () {
+elementButtonPlay.addEventListener("click", function () {
   elementCellsContainer.classList.toggle("none");
   elementMessage.classList.toggle("none");
 });
-//!TODO evento click su bottone e sulle celle (bonus)
+
+//* ciclo for per aggiungere un evento click a ogni cella
+for (let i = 0; i < cellsAll.length; i++) {
+  cellsAll[i].addEventListener("click", function () {
+    console.log(cellsAll[i].innerHTML);
+    cellsAll[i].classList.add("cell-bg");
+  });
+
+  elementButtonReset.addEventListener("click", function () {
+    elementCellsContainer.classList.add("none");
+    elementMessage.classList.remove("none");
+    cellsAll[i].classList.remove("cell-bg");
+  });
+}
