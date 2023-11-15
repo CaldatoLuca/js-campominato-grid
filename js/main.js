6cbaef15e0d63f57bac3a18f44525eb369aff829
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-15 15:43:12 +0100

    feat: creazione griglia in js senza funzioni

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+/* <div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div> */
+
+/* 
+?--------
+!FUNZIONI
+?--------
+*/
+
+/* 
+?--------
+!CODICE
+?--------
+*/
+
+const elementCellsContainer = document.querySelector(".cells-container");
+
+for (let i = 1; i <= 100; i++) {
+  const elementCell = document.createElement("div");
+  elementCell.append(i);
+  elementCell.classList.add("cell");
+  elementCellsContainer.append(elementCell);
+}
