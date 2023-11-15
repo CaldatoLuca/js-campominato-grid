60cd477f8e61c422c8c3adfe9a1809665b5da49c
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-15 18:33:32 +0100

    feat: evento click sul bottone

--- js/main.js
+++ js/main.js
@@ -26,11 +26,20 @@ function elementHtmlCreator(tag, style, content) {
 ?--------
 */
 
+//*variabili elemento html
 const elementCellsContainer = document.querySelector(".cells-container");
+const elementButton = document.querySelector(".btn");
+const elementMessage = document.getElementById("message");
 
+//* ciclo for per inserire le celle nel container (richiama la funzione che le crea e poi inserisce)
 for (let i = 1; i <= 100; i++) {
   const elementCell = elementHtmlCreator("div", "cell", i);
   elementCellsContainer.append(elementCell);
 }
 
+//* evento click sul bottone
+elementButton.addEventListener("click", function () {
+  elementCellsContainer.classList.toggle("none");
+  elementMessage.classList.toggle("none");
+});
 //!TODO evento click su bottone e sulle celle (bonus)
